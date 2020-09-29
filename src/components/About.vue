13229b6885ea63490848ac920496b9e76cb6311c
Ethan Painter <ethantpainter@gmail.com>
2020-09-28 20:30:16 -0400

    Adds Header and About vue components

---
+++ src/components/About.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="about" id="about">
+    <div class="about-heading">
+      <h2>About Me</h2>
+    </div>
+    <div class="about-content">
+      <section class="college">
+        <h3 class="college-header">
+          College
+        </h3>
+        <p>
+        </p>
+      </section>
+
+      <section class="professional"></section>
+      <h3>I'm a Full Stack Web Developer</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget
+        mauris pharetra. Risus commodo viverra maecenas accumsan. Justo nec
+        ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras. Nibh
+        venenatis cras sed felis eget velit aliquet. Eleifend mi in nulla
+        posuere sollicitudin. Mi proin sed libero enim sed faucibus turpis in
+        eu. Bibendum at varius vel pharetra vel. Viverra ipsum nunc aliquet
+        bibendum enim facilisis gravida. Sodales ut eu sem integer vitae.
+        Aliquam ut porttitor leo a diam sollicitudin tempor id eu. Gravida neque
+        convallis a cras semper auctor neque vitae. Faucibus scelerisque
+        eleifend donec pretium vulputate sapien nec sagittis. Vulputate enim
+        nulla aliquet porttitor lacus.<br />
+        <br />
+        Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium
+        vulputate. Elementum nibh tellus molestie nunc non blandit massa enim.
+        Quam lacus suspendisse faucibus interdum. Senectus et netus et
+        malesuada. Non odio euismod lacinia at quis. Quis imperdiet massa
+        tincidunt nunc pulvinar sapien et. Eget arcu dictum varius duis at
+        consectetur lorem donec. Turpis egestas pretium aenean pharetra magna ac
+        placerat vestibulum lectus.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AboutMe",
+};
+</script>
+
+<style>
+.about-heading {
+  width: 100%;
+  text-align: center;
+  margin-bottom: 30px;
+  color: #111;
+}
+.about-heading h2 {
+  font-weight: 600;
+  font-size: 30px;
+  color: #111;
+}
+.about-heading h3 {
+  width: 100%;
+}
+
+.contentBx h3 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+</style>
