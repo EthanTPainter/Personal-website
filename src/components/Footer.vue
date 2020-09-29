b8879c1bb9e73a23583283425405595e4f4c3257
Ethan Painter <ethantpainter@gmail.com>
2020-09-28 20:32:04 -0400

    Adds Contact and Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="copyright">
+    <p>Copyright &copy; {{ year }} Ethan T Painter. All Rights Reserved</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<style>
+.copyright {
+  background: #000;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+</style>
