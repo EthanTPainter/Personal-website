325f6a68cde05dd47962dd61992fdf4c9a30f42b
Ethan Painter <ethantpainter@gmail.com>
2021-08-07 23:11:14 -0400

    remove unused data

--- src/data/header.ts
+++
@@ -1,4 +0,0 @@
-export type HeaderDataType = String[];
-
-export const headerTitleData: HeaderDataType = ["Home", "Passions", "Skills & Experience", "Work", "Connect"];
-export const headerRouteData: HeaderDataType = ["/", "/passions", "/skills", "/work", "/connect"];
