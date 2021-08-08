fd27e1d9d347eb00020e252147bef2bb1d1bcacd
Ethan Painter <ethantpainter@gmail.com>
2021-08-07 23:10:56 -0400

    Adds animate css package for nice transitions between pages

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view/>
+  <router-view v-slot="{ Component }">
+    <transition
+      mode="out-in"
+      enter-active-class="animate__animated animate__fadeIn"
+      leave-active-class="animate__animated animate__fadeOut"
+    >
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 </template>
