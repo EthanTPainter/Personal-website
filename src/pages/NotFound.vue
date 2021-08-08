46c12d15cb9189fd398e0806fca8f1e44e5b0420
Ethan Painter <ethantpainter@gmail.com>
2021-08-07 23:11:43 -0400

    Remove connect page and change 404 page to not found

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-grey-darkest">
+    <h1 class="text">Uh oh. How did you get here!?</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NotFoundPage",
+  setup() {},
+});
+</script>
+
+<style scoped>
+div {
+  background: "bg-grey-darkest";
+  min-height: 100vh;
+}
+h1 {
+  color: hsl(171, 17%, 92%);
+  font-family: initial;
+  font-size: 2rem;
+  position: absolute;
+  top: 30%;
+  left: 30%;
+}
+</style>
