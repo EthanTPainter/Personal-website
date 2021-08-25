d639be617dcb4cb999f2048be438aa9a4223c6ef
Ethan <ethantpainter@gmail.com>
2021-08-25 16:26:26 -0400

    Creates intro for work page

---
+++ src/components/work/Intro.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2 class="work">Work</h2>
+  <h2 class="projects">Personal Projects</h2>
+  <h2 class="everything">And Everything In Between</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IntroWorkCard",
+  setup() {},
+});
+</script>
+
+<style scoped>
+.work {
+  color: white;
+  font-size: 8vw;
+  text-align: center;
+}
+.projects {
+  color: white;
+  font-size: 6vw;
+  text-align: center;
+}
+.everything {
+  color: white;
+  font-size: 3vw;
+  text-align: center;
+}
+</style>
